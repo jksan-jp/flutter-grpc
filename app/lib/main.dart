3be344d2ae6a0bbeda1e6bc774ba794718c286bc
jksan <jksan.jp@gmail.com>
2022-11-19 15:20:11 +0900

    update app yaml

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:grpc/grpc.dart';
+import 'package:grpc_gen/grpc_gen.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   runApp(const MyApp());
